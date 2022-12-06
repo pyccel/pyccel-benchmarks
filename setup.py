724b72bfde25bdb47195fc01debfe87712d86ac6
EmilyBourne <louise.bourne@gmail.com>
2022-12-06 16:45:40 +0100

    Add setup instructions to the repository (#5)
    Add configuration files to allow easy installation of dependencies via `pip3 install .` and add the installation instructions to the README file. Fixes #3

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='pyccel-benchmarks',
+        version='0.0.0',
+        install_requires=[
+            'pyccel',
+            'pythran',
+            'numba',
+            'pyperf',
+            'numpy',
+            'matplotlib',
+            'python_version >= "3.7"',
+        ],
+    )
