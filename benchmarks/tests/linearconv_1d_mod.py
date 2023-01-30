31e308fca5394e825f39469df70e0b4b94a042a9
Yaman Güçlü <yaman.guclu@gmail.com>
2023-01-30 15:31:09 +0100

    Update linear advection test

--- benchmarks/tests/linearconv_1d_mod.py
+++ benchmarks/tests/linearconv_1d_mod.py
@@ -8,28 +8,54 @@ Functions for solving a linear convection equation. The code is adapted from exa
 To be accelerated with pyccel or pythran
 """
 
-from numpy import zeros
-from numpy import ones
+import numpy as np
 
-# pythran export linearconv_1d()
-def linearconv_1d():
-    """ Solve the linear convection equation
+# pythran export linearconv_1d(int, float, int)
+def linearconv_1d(nx: int, dt: float, nt: int):
+    """
+    Compute an approximation of the solution u(t, x) to the 1D
+    linear advection equation
+
+        du/dt + du/dx = 0
+
+    on the domain [0, 2], with discontinuous initial conditions
+
+        u(t=0, x) = 2   for 0.5 < x < 1,
+        u(t=0, x) = 1   otherwise.
+
+    The numerical solution is computed on a uniform grid using
+    the 1st-order Godunov method, i.e. explicit Euler time stepping
+    combined with upwind fluxes.
+
+    Parameters
+    ----------
+    nx : int
+        Number of grid points in the domain.
+
+    dt : float
+        Time step size.
+
+    nt : int
+        Number of time steps to be taken.
+
+    Returns
+    -------
+    x : numpy.ndarray of nx floats
+        Spatial grid where solution is computed.
+
+    u : numpy.ndarray of nx floats
+        Numerical solution u at final time.
+
     """
 
-    #Input
-    nx=2001
-    nt=2000
-    c=1.
-    dt=0.0003
+    c  = 1.0
     dx = 2 / (nx-1)
-    u0 = ones(nx)
-    u0[int(.5 / dx):int(1 / dx + 1)] = 2
-    
-    u  = zeros(nx)
-    un = zeros(nx)
+    x  = np.linspace(0, 2, nx)
+    u  = np.ones(nx)
+    u[int(.5 / dx):int(1 / dx + 1)] = 2
 
-    u[:] = u0
     cp = c * dt / dx
+    un = np.zeros(nx)
 
     for _ in range(nt):
         un[:nx] = u[:nx]
@@ -37,4 +63,4 @@ def linearconv_1d():
         for i in range(1, nx):
             u[i] = un[i] - cp * (un[i] - un[i-1])
 
-    return u
+    return x, u
--- benchmarks/tests/numba_linearconv_1d_mod.py
+++ benchmarks/tests/numba_linearconv_1d_mod.py
@@ -9,28 +9,54 @@ To be accelerated with numba
 """
 
 from numba import njit
-from numpy import zeros
-from numpy import ones
+import numpy as np
 
 @njit(fastmath=True)
-def linearconv_1d():
-    """ Solve the linear convection equation
+def linearconv_1d(nx: int, dt: float, nt: int):
+    """
+    Compute an approximation of the solution u(t, x) to the 1D
+    linear advection equation
+
+        du/dt + du/dx = 0
+
+    on the domain [0, 2], with discontinuous initial conditions
+
+        u(t=0, x) = 2   for 0.5 < x < 1,
+        u(t=0, x) = 1   otherwise.
+
+    The numerical solution is computed on a uniform grid using
+    the 1st-order Godunov method, i.e. explicit Euler time stepping
+    combined with upwind fluxes.
+
+    Parameters
+    ----------
+    nx : int
+        Number of grid points in the domain.
+
+    dt : float
+        Time step size.
+
+    nt : int
+        Number of time steps to be taken.
+
+    Returns
+    -------
+    x : numpy.ndarray of nx floats
+        Spatial grid where solution is computed.
+
+    u : numpy.ndarray of nx floats
+        Numerical solution u at final time.
+
     """
 
-    #Input
-    nx=2001
-    nt=2000
-    c=1.
-    dt=0.0003
+    c  = 1.0
     dx = 2 / (nx-1)
-    u0 = ones(nx)
-    u0[int(.5 / dx):int(1 / dx + 1)] = 2
+    x  = np.linspace(0, 2, nx)
+    u  = np.ones(nx)
+    u[int(.5 / dx):int(1 / dx + 1)] = 2
 
-    u  = zeros(nx)
-    un = zeros(nx)
-
-    u[:] = u0
     cp = c * dt / dx
+    un = np.zeros(nx)
 
     for _ in range(nt):
         un[:nx] = u[:nx]
@@ -38,4 +64,4 @@ def linearconv_1d():
         for i in range(1, nx):
             u[i] = un[i] - cp * (un[i] - un[i-1])
 
-    return u
+    return x, u
