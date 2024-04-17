0d1f2cb415874bc389049835510f3895807adf91
EmilyBourne <louise.bourne@gmail.com>
2024-04-17 14:55:23 +0200

    Ensure Python waits until pyccel-clean has finished (#32)
    * Ensure Python waits until pyccel-clean has finished
    
    * Benchmark of pyccel
    
    * Update performance comparison
    
    * Update README and version
    
    ---------
    
    Co-authored-by: Emily Bourne <emily.bourne@epfl.ch>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -321,7 +321,7 @@ for t in tests:
                 run_units.append(possible_units.index(units))
 
         if create_shared_lib:
-            subprocess.Popen([shutil.which('pyccel-clean'), '-s'])
+            subprocess.run([shutil.which('pyccel-clean'), '-s'])
 
     if time_compilation:
         row = cell_splitter[output_format].join('{0: <25}'.format(s) for s in comp_times)
